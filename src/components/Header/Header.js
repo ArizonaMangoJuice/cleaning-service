ccb82ab5d152cb647eea74567fa28f1c89da8706
AMJ <isaelwebdeveloper@gmail.com>
2021-03-28 16:10:13 -0600

    finished basic header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,19 +8,19 @@ export default function Header() {
             </div>
             <nav className='header-nav'>
                 <div className='header-links-container'>
-                    <a className='test'>Home</a>
+                    <a href='www.google.com' className='link-decoration'>Home</a>
                 </div>
-                <div className='header-links-container '>
-                    <a className='test'>Shop</a>
-                    <div className='shop'>
-                        <a href='www.google.com' className='test'>Shop</a>    <a href='www.google.com' className='test'>Shop</a>    <a href='www.google.com' className='test'>Shop</a>    <a href='www.google.com' className='test'>Shop</a>    <a href='www.google.com' className='test'>Shop</a>
+                <div className='header-links-container ' tabIndex='0'>
+                    <a href='www.google.com' className='link-decoration'>Shop</a>
+                    <div className='shop' >
+                        <a href='www.google.com' className='link-decoration'>Shop</a>    <a href='www.google.com' className='link-decoration'>Shop</a>    <a href='www.google.com' className='link-decoration'>Shop</a>    <a href='www.google.com' className='link-decoration'>Shop</a>    <a href='www.google.com' className='link-decoration'>Shop</a>
                     </div>
                 </div>
                 <div className='header-links-container'>
-                    <a href='www.google.com' className='test'>About</a>
+                    <a href='www.google.com' className='link-decoration'>About</a>
                 </div>
                 <div className='header-links-container'>
-                    <a className='test'>Content</a>
+                    <a href='www.google.com' className='link-decoration'>Content</a>
                 </div>
             </nav>
         </header>
