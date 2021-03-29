7b9e795b0a368e83c7ab1cdac0afd9bfc015e225
AMJ <isaelwebdeveloper@gmail.com>
2021-03-28 20:27:16 -0600

    added slideshow

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.scss';
 import HeaderInfo from './components/HeaderInfo/HeaderInfo';
 import Header from './components/Header/Header';
+import Jumbotron from './components/Jumbotron/Jumbotron';
 
 function App() {
   return (
     <div className='app'>
       <HeaderInfo />
       <Header />
+      <Jumbotron />
     </div>
   );
 }
---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default function Jumbotron() {
+    return (
+        <div className='slideshow-wrapper' tabIndex='0'>
+            <div className='slideshow'>
+
+                <div className='slideshow-image-container'>
+                    <div className='content-background'></div>
+                    <div className='slideshow-content'>
+                        <h1 className='content-title'>TITLE</h1>
+                        <p className='content-info'>djsakldjska ldjsak ldasj kdlasjd klsdajk ldsaj kl</p>
+                    </div>
+                    <img className='slideshow-image test' src='https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80' />
+                </div>
+                <div className='slideshow-image-container'>
+                    <div className='content-background'></div>
+                    <div className='slideshow-content'>
+                        <h1 className='content-title'>TITLE</h1>
+                        <p className='content-info'>djsakldjska ldjsak ldasj kdlasjd klsdajk ldsaj kl</p>
+                    </div>
+                    <img className='slideshow-image' src='https://images.unsplash.com/photo-1612832164313-ac0d7e07b5ce?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' />
+                </div>
+                <div className='slideshow-image-container'>
+                    <div className='content-background'></div>
+                    <div className='slideshow-content'>
+                        <h1 className='content-title'>TITLE</h1>
+                        <p className='content-info'>djsakldjska ldjsak ldasj kdlasjd klsdajk ldsaj kl</p>
+                    </div>
+                    <img className='slideshow-image' src='https://images.unsplash.com/photo-1616627781545-86e61a33c1dd?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' />
+                </div>
+                <div className='slideshow-image-container'>
+                    <div className='content-background'></div>
+                    <div className='slideshow-content'>
+                        <h1 className='content-title'>TITLE</h1>
+                        <p className='content-info'>djsakldjska ldjsak ldasj kdlasjd klsdajk ldsaj kl</p>
+                    </div>
+                    <img className='slideshow-image' src='https://images.unsplash.com/photo-1611095780122-d692cee29291?ixid=MXwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80' />
+                </div>
+
+            </div>
+        </div>
+    )
+}
