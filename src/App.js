3df7205f48fe237b08bd44ee516aeb65d15dd07c
AMJ <isaelwebdeveloper@gmail.com>
2021-03-31 10:48:47 -0600

    get reasons component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HeaderInfo from './components/HeaderInfo/HeaderInfo';
 import Header from './components/Header/Header';
 import Jumbotron from './components/Jumbotron/Jumbotron';
 import Reason from './components/Reasons/Reasons';
+import GetQuote from './components/GetQuote/GetQuote';
+import Services from './components/Services/Services';
 
 function App() {
   return (
@@ -13,7 +15,10 @@ function App() {
       <div className='max-width'>
         <Jumbotron />
         <Reason />
+        <GetQuote />
+        <Services />
       </div>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Services/Services.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+export default function Services() {
+    return (
+        <section className='services'>
+            <section className='title-and-services'>
+                <h2 className='services-title'>Get Satisfied with the services we provide A to Z in Cleaning</h2>
+                <ul className='services-list-container'>
+                    <li className='services-list'>
+                        <img className='services-list-image' src='https://dtweclean.wpengine.com/wp-content/uploads/2016/02/list-img2.png' />
+                        <span>Apartment Cleaning</span>
+                    </li>
+                    <li className='services-list'>
+                        <img className='services-list-image' src='https://dtweclean.wpengine.com/wp-content/uploads/2016/02/list-img3.png' />
+                        <span>Move In/Move Out Cleaning</span>
+                    </li>
+                    <li className='services-list'>
+                        <img className='services-list-image' src='https://dtweclean.wpengine.com/wp-content/uploads/2016/02/list-img5.png' />
+                        <span>Window washing</span>
+                    </li>
+                    <li className='services-list'>
+                        <img className='services-list-image' src='https://dtweclean.wpengine.com/wp-content/uploads/2016/02/list-img6.png' />
+                        <span>Construction Clean-Up</span>
+                    </li>
+                    <li className='services-list'>
+                        <img className='services-list-image' src='https://dtweclean.wpengine.com/wp-content/uploads/2016/02/list-img7.png' />
+                        <span>
+                            Office Cleaning
+                        </span>
+                    </li>
+                </ul>
+            </section>
+            <section className='services-image-container'>
+                <section className='services-image-transition-container'>
+                    {/* <div className='services-main-image'>
+
+                    </div> */}
+                    <img className='services-main-image' src='https://images.unsplash.com/photo-1581578731548-c64695cc6952?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80' />
+                </section>
+            </section>
+        </section>
+    )
+}
