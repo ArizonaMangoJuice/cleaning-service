0b046c1cb5f3fa9933c193967c77da04eddda7b2
AMJ <isaelwebdeveloper@gmail.com>
2021-04-01 08:17:30 -0600

    seperated components to own styling file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.scss';
 import HeaderInfo from './components/HeaderInfo/HeaderInfo';
 import Header from './components/Header/Header';
 import Jumbotron from './components/Jumbotron/Jumbotron';
